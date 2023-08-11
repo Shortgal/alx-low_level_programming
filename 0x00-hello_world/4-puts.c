d9c42fd799ecfc53025f51d097adb43e91de4c2b
Kgothatso <tshokekp@gmail.com>
2023-08-11 05:24:46 -0700

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,10 @@
 #include <stdio.h>
 /**
-*main - Entry point
-*
-*Description: using puts function 
-*Return: Always 0 (Success)
-*/
+ *main - Entry point
+ *
+ *Description: using puts function 
+ *Return: Always 0 (Success)
+ */
 int main(void)
 {
        puts("\"Programming is like building a multilingual puzzle");
