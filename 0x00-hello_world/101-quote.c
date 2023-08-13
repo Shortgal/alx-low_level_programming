3752912dd526b097715e81e302e0c94d7dfe1e51
Kgothatso <tshokekp@gmail.com>
2023-08-13 14:25:03 -0700

    101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -3,11 +3,11 @@
 /**
  * main - Entry point
  *
- * Return: Always 1 (Success)
+ * Return: 1
  */
 int main(void)
 {
        
-        printf("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n",59;
+        write(2,"and that piece of art is useful\" - Dora Korpar, 2015-10-19",59);
 	return (1);
 }
