27a0c511e4ae28c42e67954c315e269a7623c34c
Kgothatso <tshokekp@gmail.com>
2023-08-15 06:39:32 -0700

    0-putchar.c

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Sucess)
+ */
+
+int main(void)
+{
+	_putchar('_');
+	_putcahr('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putcahr('h');
+	_putchar('a');
+	_putchar('r');
+
+	return (0);
+}
