131db2c0d7f67b131b5856aa00771439fc847335
Kgothatso <tshokekp@gmail.com>
2023-08-17 04:22:13 -0700

    102-fibonacci.c

--- 0x02-functions_nested_loops/102-fibonacci.c
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -9,12 +9,11 @@ int main(void)
 
 {
 	int count;
-	unsigned long fib1 = 0, fib2 = sum;
+	unsigned long fib1 = 0, fib2 = 1, sum;
 
 	for (count = 0; count < 50; count++)
 	{
 	sum = fib1 + fib2;
-
 	printf("%lu", sum);
 
 	fib1 = fib2;
