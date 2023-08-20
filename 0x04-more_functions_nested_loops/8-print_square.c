70d36fd6b1d12e65629d16695e508b863801a4c2
Kgothatso <tshokekp@gmail.com>
2023-08-20 06:47:14 -0700

    8-print_square.c

--- 0x04-more_functions_nested_loops/8-print_square.c
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -3,6 +3,8 @@
 /**
  * print_square - prints a square, followed by a new line
  * @size: size of the square
+ *
+ * Return: void
  */
 void print_square(int size)
 {
@@ -15,7 +17,6 @@ void print_square(int size)
 	{
 		for (j = 0; j < (size); j++)
 		{
-
 			_putchar('#');
 		}
 		_putchar('\n');
