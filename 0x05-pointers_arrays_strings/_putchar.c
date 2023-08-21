6a3288494cd8f08e17f6b762c4d44d33f337314d
Kgothatso <tshokekp@gmail.com>
2023-08-21 10:10:53 -0700

    3-puts.c

--- 0x05-pointers_arrays_strings/_putchar.c
+++ 0x05-pointers_arrays_strings/_putchar.c
@@ -1,1 +1,14 @@
-Pointers, arrays and strings
+#include "main.h"
+#include <unistd.h>
+/**
+ * _putchar - writes the character c to stoudt
+ * @c: The character to print
+ *
+ * Return: On success 1
+ *
+ * On error, -1 is returned, and errno is set appropriately
+ */
+int _putchar(char c)
+{
+	return (write(1, &c, 1));
+}
