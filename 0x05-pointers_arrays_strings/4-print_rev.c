9e67f0b05d4bc9a458ca5be58c9d571263cdc1bc
Kgothatso <tshokekp@gmail.com>
2023-08-21 10:32:14 -0700

    4-print_rev.c

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,24 @@
+#include "main.h"
+/**
+ * print_rev - prints a string in reserve followed by a new line
+ * @s: string
+ * Return: 0
+ */
+void print_rev(char *s)
+{
+	int tall = 0;
+	int o;
+
+	while (*s != '\0')
+	{
+		tall++;
+		s++;
+	}
+	s--;
+	for (o = tall; o > 0; o--)
+	{
+		_putchar(*s);
+		s--;
+	}
+	_putchar('\n');
+}
