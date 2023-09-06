6f28a6504e729ea449fcda9de0524fa60dd56516
Kgothatso <tshokekp@gmail.com>
2023-09-06 05:58:32 -0700

    3-mul.c

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -2,7 +2,7 @@
 #include "main.h"
 
 /**
- * _atoi - converts a string to an ineger
+ * _atoi - converts a string to an integer
  * @s: string to be converted
  *
  * Return: the int to be converted from the string
