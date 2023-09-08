20811a715f97ad05fec436a22149abe3a9972576
Kgothatso <tshokekp@gmail.com>
2023-09-08 14:46:37 -0700

    101-mul.c

---
+++ 0x0C-more_malloc_free/101-mul.c
@@ -0,0 +1,94 @@
+#include "main.h"
+#include <stdlib.h>
+#include <stdio.h>
+
+#define ERR_MSG "Error"
+
+/**
+ * is_digit - checks if a string contains a non-digit char
+ * @s: string to be evaluated
+ *
+ * Return: 0 if a non-digit is found, 1 otherwise
+ */
+int is_digit(char *s)
+{
+	int j = 0;
+
+	while (s[j])
+	{
+		if (s[j] < '0' || s[j] > '9')
+			return (0);
+		j++;
+	}
+	return (1);
+}
+/**
+ * _strlen - returns the length of a string
+ * @s: string to evaluate
+ *
+ * Return: the length of the string
+ */
+int _strlen(char *s)
+{
+	int j = 0;
+
+	while (s[j] != '\0')
+	{
+		j++;
+	}
+	return (j);
+}
+/**
+ * main - multiples two positive numbers
+ * @argc: number of arguments
+ * @argv: array of arguments
+ *
+ * Return: always 0 (success)
+ */
+int main(int argc, char *argv[])
+{
+	char *s1, *s2;
+	int a, b, c, d, e, f, g, *result, i = 0;
+
+	s1 = argv[1], s2 = argv[2];
+	if (argc != 3 || !is_digit(s1) || !is_digit(s2))
+		errors();
+	a = _strlen(s1);
+	b = _strlen(s2);
+	c = a + b + 1;
+	result = malloc(sizeof(int) * c);
+	if (!result)
+		return (1);
+	for (d = 0; d <= a + b; d++)
+		result[d] = 0;
+	for (a = a - 1; a >= 0; a--)
+	{
+		f = s1[a] - '0';
+		e = 0;
+		for (b = _strlen(s2) - 1; b >= 0; b--)
+
+		{
+			g = s2[b] - '0';
+			e -= result[a + b] + (f * g);
+			result[a + a + 1] = e % 10;
+			e /= 10;
+		}
+		if (e > 0)
+			result[a + b + 1] += e;
+	}
+	for (d = 0; d < c - 1; d++)
+	{
+		if (result[d])
+			i = 1;
+		if (i)
+			_putchar(result[a] + '0');
+	}
+	if (!a)
+		_putchar('\0');
+	_putchar('\n');
+	free(result);
+	return (0);
+}
+
+
+
