1c70983254bc6a7d07f36c5163e2817cf74df488
Kgothatso <tshokekp@gmail.com>
2023-09-10 12:17:21 -0700

    101-mul.c

--- 0x0C-more_malloc_free/101-mul.c
+++ 0x0C-more_malloc_free/101-mul.c
@@ -59,47 +59,43 @@ void errors(void)
 int main(int argc, char *argv[])
 {
 	char *s1, *s2;
-	int a, b, c, d, e, f, g, *result, i = 0;
+	int len1, len2, len, i, carry, digit1, digit2, *result, a = 0;
 
 	s1 = argv[1], s2 = argv[2];
 	if (argc != 3 || !is_digit(s1) || !is_digit(s2))
 		errors();
-	a = _strlen(s1);
-	b = _strlen(s2);
-	c = a + b + 1;
-	result = malloc(sizeof(int) * c);
+	len1 = _strlen(s1);
+	len2 = _strlen(s2);
+	len = len1 + len2 + 1;
+	result = malloc(sizeof(int) * len);
 	if (!result)
 		return (1);
-	for (d = 0; d <= a + b; d++)
-		result[d] = 0;
-	for (a = a - 1; a >= 0; a--)
+	for (i = 0; i <= len1 + len2; i++)
+		result[i] = 0;
+	for (len1 = len1 - 1; len2 >= 0; len1--)
 	{
-		f = s1[a] - '0';
-		e = 0;
-		for (b = _strlen(s2) - 1; b >= 0; b--)
-
+		digit1 = s1[len1] - '0';
+		carry = 0;
+		for (len2 = _strlen(s2) - 1; len2 >= 0; len2--)
 		{
-			g = s2[b] - '0';
-			e += result[a + b + 1] + (f * g);
-			result[a + a + 1] = e % 10;
-			e /= 10;
+			digit2 = s2[len2] - '0';
+			carry += result[len1 + len2 + 1] + (digit1 * digit2);
+			result[len1 + len2 + 1] = carry % 10;
+			carry /= 10;
 		}
-		if (e > 0)
-			result[a + b + 1] += e;
+		if (carry > 0)
+			result[len1 + len2 + 1] += carry;
 	}
-	for (d = 0; d < c - 1; d++)
+	for (i = 0; i < len - 1; i++)
 	{
-		if (result[d])
-			i = 1;
+		if (result[i])
+			a = 1;
 		if (i)
-			_putchar(result[d] + '0');
+			_putchar(result[i] + '0');
 	}
-	if (!i)
+	if (!a)
 		_putchar('0');
 	_putchar('\n');
 	free(result);
 	return (0);
 }
-
-
-
