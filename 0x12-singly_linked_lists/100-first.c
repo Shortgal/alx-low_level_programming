0ae3eda5a5ea71f19cb0116a796b06c810ff2779
Kgothatso <tshokekp@gmail.com>
2023-09-21 11:37:48 -0700

    101-hello_holberton.asm

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main(void)
+
+/**
+ * first -  prints You're beat! and yet, you must allow,\nI bore my house upon 
+ * my back!\n before the main
+ */
+int main(void)
+{
+	printf("You're beat! and yet, you must allow,\n");
+	printf("I bore my house upon my back!\n");
+}
