6d89a17060a49917c9d293d00e8b057fc06f2397
Kgothatso <tshokekp@gmail.com>
2023-09-27 13:33:08 -0700

    101-print_listint_safe.c

--- 0x13-more_singly_linked_lists/101-print_listint_safe.c
+++ 0x13-more_singly_linked_lists/101-print_listint_safe.c
@@ -76,8 +76,10 @@ size_t print_listint_safe(const listint_t *head)
 			printf("[%p]%d\n", (void *)head, head->n);
 			head = head->next;
 		}
-		return (nodes);
+
+		printf("->[%p]%d\n", (void *)head, head->n);
 	}
+		return (nodes);
 }
 
 
