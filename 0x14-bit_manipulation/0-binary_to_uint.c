b7e674d2e7e6c14c05367dfc1e68be335ea139d6
Kgothatso <tshokekp@gmail.com>
2023-09-29 07:22:03 -0700

    5-flip_bits.c

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -13,11 +13,13 @@ unsigned int binary_to_uint(const char *b)
 
 	if (!b)
 		return (0);
+
 	for (i = 0; b[i]; i++)
 	{
 		if (b[i] < '0' || b[i] > '1')
 			return (0);
 		j = 2 * j + (b[i] - '0');
 	}
+	
 	return (j);
 }
--- 0x14-bit_manipulation/5-flip_bits.c
+++ 0x14-bit_manipulation/5-flip_bits.c
@@ -6,20 +6,21 @@
  * @n: first number
  * @m: second number
  *
+ * Return: the number of bits you would to flip
  */
 unsigned int flip_bits(unsigned long int n, unsigned long int m)
 {
-	int i, p;
-	unsigned long int j;
-	unsigned long int flip = n ^ m;
+	unsigned long int result;
+	unsigned long int count;
 
-	for (i = 63; i >= 0; i--)
+	result = n ^ m;
+	for (count = 0; result > 0;)
 	{
-		j = flip >> i;
-		if (j & 1)
-			p++;
+		if ((result & 1) == 1)
+			count++;
+		result = result >> 1;
 	}
 
-	return (p);
+	return (count);
 }
 
